a5282ea2096cf0d0e9fc7c53d7b3858204395ba6
Warren Villarosa <58594754+daysthroughcode@users.noreply.github.com>
2021-02-23 20:52:14 +0800

    API endpoint working. Ready for Deployment

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,88 +1,39 @@
-from flask import Flask, request, render_template, redirect,jsonify,render_template
-app = Flask(__name__)
-
-#Imnport Initial Dependencies
-
-import sqlite3
-import pandas as pd
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
 import numpy as np
 
-#retrieve data from SQLite DB
+app = Flask(__name__)
 
-conn = sqlite3.connect(r'C:\Users\wvill\Documents\GitHub\DABProject3\Dashboard\resources\wine_quality.db')
-query = 'Select * from wine_quality'
-df = pd.read_sql_query(query,conn)
-conn.close()
+#Import Logistic Regression Model
+lrmodel=pickle.load(open('lrsommelier.pkl','rb'))
 
-#Set Y
-y = df.categorical_quality
-y = y.values.reshape(-1,1)
+#Import Random Forest Model 
+rfmodel=pickle.load(open('rfsommelier.pkl','rb'))
 
-#Set X
-X = df.drop('categorical_quality', axis=1).values
-
-#Import Test Train Split
-from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25)
-
-#Scale Values
-from sklearn.preprocessing import StandardScaler
-scl=StandardScaler()
-scl.fit_transform(X,y)
-
-#Logistic Regression
-from sklearn.linear_model import LogisticRegression
-#Create and Train Logistic Regression Model
-
-lr=LogisticRegression()
-lr.fit(X_train,y_train.ravel())
-
-#Random Forest
-from sklearn.ensemble import RandomForestClassifier
- # Create and Fit Random Forest Model
-
-rfc_r=RandomForestClassifier(n_estimators=500,criterion='entropy',max_features=6,max_depth=10,random_state=42)
-rfc_r.fit(X_train,y_train.ravel())
-   
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+def hello_world():
+    return render_template("index.html")
 
-@app.route('/lrmodel', methods=['GET','POST'])
-def lrmodel():
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    print(int_features)
+    print(final)
+    lrprediction=lrmodel.predict(final)
+    lrresult = ()
+    rfresult = ()
+    if lrprediction == 0:
+        lrresult = "High"
+    else:
+        lrresult = "Low"
+    rfprediction=rfmodel.predict(final)
+    if rfprediction == 0:
+        rfresult = "High"
+    else:
+        rfresult = "Low"
 
-    if not request.json:
-        abort(400)
-    input = {
-        'VolatileAcidity': request.json['fieldA'],
-        'ResidualSugar': request.json['fieldB'],
-        'Chlorides': request.json['fieldC'],
-        'Density': request.json['fieldD'],
-        'pH': request.json['fieldE'],
-        'Sulphates': request.json['fieldF'],
-        'Alcohol': request.json['fieldG']
-    }
-    result = lr.predict(input)
-    return jsonify({'result': result}), 200 # return success
+    return render_template('index.html',pred=f'The Quality of your wine is {lrresult} according to the Logistic Regression model and {rfresult} according to the Random Forest Classifier')
 
-@app.route('/rfmodel', methods=['POST'])
-def rfmodel():
-
-    if not request.json:
-        abort(400)
-    input = {
-        'VolatileAcidity': request.json['fieldA'],
-        'ResidualSugar': request.json['fieldB'],
-        'Chlorides': request.json['fieldC'],
-        'Density': request.json['fieldD'],
-        'pH': request.json['fieldE'],
-        'Sulphates': request.json['fieldF'],
-        'Alcohol': request.json['fieldG']
-    }
-    result = rfc_r.predict(input)
-    return jsonify({'result': result}), 200 # return success
-
-    if __name__ == "__main__":
-        app.debug=True
-        app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
